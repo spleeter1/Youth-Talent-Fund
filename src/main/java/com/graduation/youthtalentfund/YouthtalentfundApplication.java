3665ed0961f7123debb873ba6936552819d2b0a1
namnd4 <namnd4@rikkeisoft.com>
2025-10-15 14:46:48 +0700

    init project

---
+++ src/main/java/com/graduation/youthtalentfund/YouthtalentfundApplication.java
@@ -0,0 +1,13 @@
+package com.graduation.youthtalentfund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthtalentfundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouthtalentfundApplication.class, args);
+	}
+
+}
